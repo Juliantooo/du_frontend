04368a1cd8766c442fb8b04419bf5abf3cacfb19
faJaar <slashcrackdie@gmail.com>
2020-03-02 20:24:20 +0700

    init commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,30 @@
 import React from "react"
+import Menu from "../components/navbar"
+import Hero from "../components/hero"
+import heroBg from "../assets/Group90.png"
+import Kelas from "../components/listKelas"
+import Pemateri from "../components/pemateri/pemateri"
+import InfoRegis from "../components/info_regis/infoRegis"
+import Jadwal from "../components/jadwal/jadwal"
+import Kontak from "../components/kontak/kontak"
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div
+      className="container-fluid"
+      style={{
+        backgroundImage: `url(${heroBg})`,
+        backgroundSize: "100% 1000px",
+        backgroundRepeat: "no-repeat"
+      }}
+    >
+      <Menu />
+      <Hero />
+      <Kelas />
+      <Pemateri />
+      <InfoRegis />
+      <Jadwal />
+      <Kontak />
     </div>
   )
 }
