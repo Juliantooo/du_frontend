622f2fe921b68acbf760540d3cfe9de2d220349c
faJaar <slashcrackdie@gmail.com>
2020-03-03 14:01:30 +0700

    add testimoni

---
+++ src/components/testimoni/testimoni.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styles from "../../styles/styles.module.css"
+
+const Testimoni = () => {
+  return (
+    <div className={`${styles.testi_height} container `}>
+      <div className={`${styles.headline} title pt-5 mb-5`}>
+        Kontak
+        <hr className={styles.underline} />
+      </div>
+      ....
+    </div>
+  )
+}
+
+export default Testimoni
