75d43e247697feff0599bd2f262a023abe07609d
Juliantooo <slashcrackdie@gmail.com>
2020-03-09 23:39:56 +0700

    perbaikan layout mobile

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -8,21 +8,20 @@ import igLogo from "../../assets/instagram.png"
 
 const Footer = () => {
   return (
-    <div className="container-fluid " style={{ height: "35vh" }}>
+    <div className={`${styles.footerScreen} container-fluid `}>
       <Row className="justify-content-center">
         <Col xs="6" sm="6" md="5">
           <div className="about_doscom">
             <img
-              className="d-flex flex-column"
+              className={`${styles.doscom_logo} d-flex flex-column mb-2`}
               src={doscomLogo}
-              style={{ width: "10%" }}
               alt=""
             />
             <div className={styles.about_doscom}>
               <p className={styles.subheadline} style={{ fontWeight: "bold" }}>
                 Tentang DOSCOM
               </p>
-              <p className={styles.sosmed}>
+              <p className={styles.about}>
                 Kami adalah komunitas open source di bidang perangkat lunak yang
                 memilik visi untuk Memasyarakatkan Open Source dan Meng Open
                 Source-kan Masyrakat melalui berbagai kegitan seperti workshop,
@@ -33,7 +32,13 @@ const Footer = () => {
           </div>
         </Col>
         <Col xs="6" sm="6" md="5">
-          <div className={`${styles.sosmed} text-right mt-4`}>
+          <div
+            className={
+              window.innerWidth <= 700
+                ? `${styles.sosmed} text-right mt-4 pt-3`
+                : `${styles.sosmed} text-right`
+            }
+          >
             <p className={styles.subheadline} style={{ fontWeight: "bold" }}>
               Social Media
             </p>
@@ -54,7 +59,7 @@ const Footer = () => {
                 alt=""
               />
             </span>
-            <div className={`my-4 ${styles.sosmed}`}>
+            <div className={`my-3 ${styles.sosmed_text}`}>
               <p>doscomedia@gmail.com</p>
               <p>www.doscom.org</p>
               <p>Copyright doscom 2020</p>
