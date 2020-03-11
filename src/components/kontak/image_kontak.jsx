25c5915361b5f491fa99ab879f3dca7d8260c5e2
Juliantooo <slashcrackdie@gmail.com>
2020-03-11 22:31:52 +0700

    add imageCP components

---
+++ src/components/kontak/image_kontak.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import imgDummy from "../../assets/Android.png"
+import styles from "../../styles/styles.module.css"
+
+const ImageKontak = () => {
+  return (
+    <div>
+      <img className={styles.imgCP} src={`${imgDummy}`} alt="" />
+    </div>
+  )
+}
+
+export default ImageKontak
