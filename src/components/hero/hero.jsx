2437c18b0f2d5473d0c82f5619e4083ad9b140d2
Juliantooo <slashcrackdie@gmail.com>
2020-03-11 22:32:35 +0700

    edit

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,33 +1,21 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { Row, Col } from "reactstrap"
 import { DaftarBtn } from "../button"
 import styles from "../../styles/styles.module.css"
-import ilustrasi from "../../assets/logo1.png"
 
 const Hero = () => {
-  useEffect(() => {
-    if (window.innerWidth <= 700) {
-      document
-        .getElementsByClassName("row")[0]
-        .appendChild(document.getElementsByClassName("ilu_hero")[0])
-      document
-        .getElementsByClassName("row")[0]
-        .appendChild(document.getElementsByClassName("intro_hero")[0])
-    }
-  })
+  const classDU =
+    window.innerWidth <= 700
+      ? `w-100 mx-auto text-white text-center`
+      : `w-75 mx-auto text-white text-left`
 
   return (
     <div
-      className={`${styles.halfscreen} Hero align-items-center d-flex container-fluid mb-5 pb-5`}
+      className={`${styles.fullscreen} Hero align-items-center d-flex container-fluid`}
     >
       <Row>
-        <Col
-          xs="12"
-          sm="12"
-          md="6"
-          className="text-left justify-content-center intro_hero"
-        >
-          <div className=" w-75 mx-auto text-white mt-5 pt-5">
+        <Col xs="12" sm="12" md="8" className="justify-content-center">
+          <div className={classDU}>
             <div className={`${styles.headline}`}>
               AYO KEMBANGKAN <span style={{ color: "lightgreen" }}>MINAT</span>{" "}
               &<span style={{ color: "lightgreen" }}>BAKATMU</span>
@@ -41,14 +29,6 @@ const Hero = () => {
               <DaftarBtn>Daftar</DaftarBtn>
             </div>
           </div>
-        </Col>
-        <Col
-          xs="12"
-          sm="12"
-          md="6"
-          className="justify-content-center mb-5 ilu_hero"
-        >
-          <img src={ilustrasi} alt="" className={styles.ilustrasi} />
         </Col>
       </Row>
     </div>
