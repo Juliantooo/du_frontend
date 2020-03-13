7eff8b1af328e7dc3eb629c5f0286f7d1284aac3
Juliantooo <slashcrackdie@gmail.com>
2020-03-13 09:46:28 +0700

    perbaikan component

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -4,21 +4,23 @@ import { DaftarBtn } from "../button"
 import styles from "../../styles/styles.module.css"
 
 const Hero = () => {
+  const classHero =
+    window.innerWidth <= 700
+      ? `align-items-center h-75 d-flex`
+      : `align-items-center h-75 d-flex`
   const classDU =
     window.innerWidth <= 700
-      ? `w-100 mx-auto text-white text-center`
+      ? `w-75 mx-auto text-white text-center`
       : `w-75 mx-auto text-white text-left`
 
   return (
-    <div
-      className={`${styles.fullscreen} Hero align-items-center d-flex container-fluid`}
-    >
-      <Row>
+    <div className={`${styles.fullscreen} Hero container-fluid`}>
+      <Row className={classHero}>
         <Col xs="12" sm="12" md="8" className="justify-content-center">
           <div className={classDU}>
-            <div className={`${styles.headline}`}>
-              AYO KEMBANGKAN <span style={{ color: "lightgreen" }}>MINAT</span>{" "}
-              &<span style={{ color: "lightgreen" }}>BAKATMU</span>
+            <div className={`${styles.hero_intro}`}>
+              AYO KEMBANGKAN <span style={{ color: "lightgreen" }}>MINAT </span>{" "}
+              &<span style={{ color: "lightgreen" }}> BAKATMU </span>
               BERSAMA DOSCOM
             </div>
             <div className={`${styles.subheadline}`}>
@@ -26,7 +28,7 @@ const Hero = () => {
                 Training dengan metode yang memadukan beberapa kelas dan
                 pengajar yang menyenangkan sesuai minat peserta
               </p>
-              <DaftarBtn>Daftar</DaftarBtn>
+              <DaftarBtn>Daftar Sekarang</DaftarBtn>
             </div>
           </div>
         </Col>
