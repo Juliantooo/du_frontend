9f96c34e2443075fb5e5ae3200fd135a65a1d4d1
Juliantooo <slashcrackdie@gmail.com>
2020-03-15 12:12:09 +0700

    perbaikan component

--- src/components/form/sideForm.jsx
+++ src/components/form/sideForm.jsx
@@ -1,10 +1,10 @@
 import React from "react"
-import sideFormImg from "../../assets/Group70"
-
+import sideFormImg from "../../assets/Group70.png"
+import styles from "../../styles/styles.module.css"
 const SideForm = () => {
   return (
     <div className="img">
-      <img src={sideFormImg} alt="" />
+      <img src={sideFormImg} className={`${styles.sideFormIlu}`} alt="" />
     </div>
   )
 }
--- src/pages/registrasi.jsx
+++ src/pages/registrasi.jsx
@@ -1,9 +1,16 @@
 import React from "react"
+import { Row, Col } from "reactstrap"
+import SideForm from "../components/form/sideForm"
 
 const Registrasi = () => {
   return (
-    <div style={{ height: "100vh", backgroundColor: "red" }}>
-      untuk regis from
+    <div style={{ height: "100vh" }}>
+      <Row style={{ margin: "0", padding: "0" }}>
+        <Col xs="6" sm="6" md="3" style={{ padding: "0" }}>
+          <SideForm />
+        </Col>
+        <Col xs="6" sm="6" md="9" style={{ padding: "0" }}></Col>
+      </Row>
     </div>
   )
 }
